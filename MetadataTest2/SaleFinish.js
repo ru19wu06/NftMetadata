841bf0076f1eb38e740c55281d820aa0a27b4b7a
unknown <ru19wu06@gmail.com>
2021-09-22 15:33:41 +0800

    更新圖片

---
+++ MetadataTest2/SaleFinish.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+const { finished } = require('stream');
+ 
+
+
+// "name": "Solana FishDoggie",
+// "description": "A Cute FishDoge, Can do nothing",
+// "image": "https://ru19wu06.github.io/FishDoge_FDOGE/MetadataFF/2x/test.jpg",
+
+
+
+
+
+for(var a=1;a<=6;a++){
+
+    var fileName = String(a);
+
+    var names = 'test'+a;
+    var description = 'a fsfs';
+    var image = 'https://ru19wu06.github.io/NftMetadata/MetadataTest3/1x/';
+    //Fdoggie1.png
+    image = image+'Fdoggie'+a+'.png' 
+
+    var obj = {
+        name:names,
+        description:description,
+        image:image
+    };
+
+
+    
+  
+    
+fs.writeFile(fileName,  JSON.stringify(obj), function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Write operation complete.');
+});
+
+//console.log(JSON.stringify(obj));
+
+}
+
+//https://ru19wu06.github.io/FishDoge_FDOGE/Metadata_test/
