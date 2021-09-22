7bea8ec04c4c44f0d9f010cbb258c98da5379d31
unknown <ru19wu06@gmail.com>
2021-09-22 15:21:25 +0800

    最後測試

---
+++ MetadataTest3/rewrite.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+const { finished } = require('stream');
+ 
+
+
+// "name": "Solana FishDoggie",
+// "description": "A Cute FishDoge, Can do nothing",
+// "image": "https://ru19wu06.github.io/FishDoge_FDOGE/MetadataFF/2x/test.jpg",
+
+
+
+
+
+for(var a=1;a<=10;a++){
+
+    var fileName = String(a);
+    var names = 'test'+a;
+    var description = 'a fsfs';
+    var image = 'https://ru19wu06.github.io/NftMetadata/MetadataTest/www.jpg';
+
+    var obj = {
+        name:names,
+        description:description,
+        image:image
+    };
+
+
+    
+  
+    
+fs.writeFile(fileName,  JSON.stringify(obj), function (err) {
+    if (err)
+        console.log(err);
+    else
+        console.log('Write operation complete.');
+});
+
+//console.log(JSON.stringify(obj));
+
+}
+
+//https://ru19wu06.github.io/FishDoge_FDOGE/Metadata_test/
